9e98293d00dd8f36b82245658686681139fa8ca7
zmrwego <13240441916@126.com>
2019-09-12 15:27:38 +0800

    更新lru

--- src/main/java/LRU/ZclLru.java
+++ src/main/java/LRU/ZclLru.java
@@ -1,88 +1,70 @@
 package LRU;
-
 import java.util.HashMap;
+import java.util.Map;
 
 public class ZclLru {
 
 
-    public ZclLru(int capacity) {
-        this.size = 0;
+    class Node{
+        Node prev;
+        Node next;
+        int value;
+
+    }
+    private Node head,tail;
+    private int capacity,size;
+    private Map<Integer,Node> map = new HashMap<>();
+    public ZclLru(int capacity){
         this.capacity = capacity;
-        head = new DlinkedNode();
-        tail = new DlinkedNode();
+        size = 0;
+        head = new Node();
+        tail = new Node();
         head.next = tail;
         tail.prev = head;
     }
 
-    public int get(int key) {
-        DlinkedNode node = cache.get(key);
-        if (node==null) return -1;
-        moveToHead(node);
-        return node.value;
+    //尾插
+    public void add(Node node){
+        Node temp = tail.prev;
+        temp.next = node;
+        node.prev = temp;
+        node.next = tail;
     }
-
-    public void put(int key, int value) {
-        DlinkedNode node = cache.get(key);
-        if (node == null) {
-            DlinkedNode newNode = new DlinkedNode();
-            newNode.key = key;
-            newNode.value = value;
-            cache.put(key, newNode);
-            add(newNode);
-            ++size;
-        }
-        if (size > capacity) {
-            DlinkedNode node1 = removeEldest();
-            //map中要和链表同步
-            cache.remove(node1.value);
-            --size;
-        } else {
-            node.value = value;
-            moveToHead(node);
-        }
+    //删除头元素
+    public void removeFirst(){
+        Node next = head.next;
+        head.next = next.next;
+        next.next.prev = head;
     }
-
-    class DlinkedNode {
-        int key;
-        int value;
-        DlinkedNode prev;
-        DlinkedNode next;
-    }
-    private DlinkedNode head, tail;
-    private int size, capacity;
-    private HashMap<Integer, DlinkedNode> cache = new HashMap<>();
-    //总是插入head的右端
-    public void add(DlinkedNode node) {
-        node.prev = head;
-        node.next = head.next;
-        head.next.prev = node;
-        head.next = node;
-    }
-
-    public void remove(DlinkedNode node) {
-        DlinkedNode pre = node.prev;
-        DlinkedNode next = node.next;
-        pre.next = next;
-        next.prev = pre;
-    }
-
-    public void moveToHead(DlinkedNode node) {
-        remove(node);
+    //更新一个元素  保证最新元素在链表尾部
+    public void update(Node node){
+        Node temp = node.prev;
+        temp.next = node.next;
+        node.next.prev = temp;
         add(node);
     }
-
-    public DlinkedNode removeEldest() {
-        DlinkedNode last = tail.prev;
-        remove(last);
-        return last;
+    //执行lru的get操作
+    public int get(int key){
+        Node node = map.get(key);
+        if (node != null) {
+            update(node);
+            return node.value;
+        }
+        return 0;
     }
+    //执行put操作
+    public void put(int key,int value){
+        Node node = new Node();
+        node.value = value;
+        size++;
+        map.put(key, node);
+        if (size > capacity) {
+            removeFirst();
+            size--;
+        }
+        add(node);
 
-
-
-
-//    public
-
-
+    }
 
 
 }
